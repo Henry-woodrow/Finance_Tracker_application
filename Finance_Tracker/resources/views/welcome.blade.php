c17e6f35af81cc7b51281adcd4d9d40369d0e1e2
Henry-woodrow <93534630+Henry-woodrow@users.noreply.github.com>
2025-02-06 18:06:09 +0000

    First commit

---
+++ Finance_Tracker/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+    @include('layouts.header')
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline text-white bg-blue-500 border-yellow-300 border-2">
+      Hello world!
+    </h1>
+  </body>
+</html>
